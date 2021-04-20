93f7aba7bcee6568073bea12382878e4a35c2ac2
Gleydson Epifanio <gleydsonnnn@outlook.com>
2021-04-20 03:40:22 -0300

    novo

--- src/test/java/br/ce/wcaquino/taskbackend/controller/TasksControllerTest.java
+++ src/test/java/br/ce/wcaquino/taskbackend/controller/TasksControllerTest.java
@@ -28,41 +28,40 @@ public class TasksControllerTest {
 	}
 
 	@Test
-	public void naoDeveSalvarTarefaSemDescricao()  {
+	public void naoDeveSalvarTarefaSemDescricao() {
 		Task todo = new Task();
 		todo.setDueDate(LocalDate.now());
 		try {
 			controller.save(todo);
-			Assert.fail("Nao deveria chegar nesse ponto!");
+			Assert.fail("Não deveria chegar nesse ponto!");
 		} catch (ValidationException e) {
 			Assert.assertEquals("Fill the task description", e.getMessage());
 		}
-	}		
-		
+	}
+	
 	@Test
 	public void naoDeveSalvarTarefaSemData() {
 		Task todo = new Task();
 		todo.setTask("Descricao");
-//		todo.setDueDate(LocalDate.now());
 		try {
 			controller.save(todo);
-			Assert.fail("Nao deveria chegar nesse ponto!");
+			Assert.fail("Não deveria chegar nesse ponto!");
 		} catch (ValidationException e) {
 			Assert.assertEquals("Fill the due date", e.getMessage());
-		}		
+		}
 	}
 	
 	@Test
-	public void naoDeveSalvarTarefaComData() {
+	public void naoDeveSalvarTarefaComDataPassada() {
 		Task todo = new Task();
 		todo.setTask("Descricao");
 		todo.setDueDate(LocalDate.of(2010, 01, 01));
 		try {
 			controller.save(todo);
-			Assert.fail("Nao deveria chegar nesse ponto!");
+			Assert.fail("Não deveria chegar nesse ponto!");
 		} catch (ValidationException e) {
 			Assert.assertEquals("Due date must not be in past", e.getMessage());
-		}	
+		}
 	}
 	
 	@Test
@@ -71,7 +70,7 @@ public class TasksControllerTest {
 		todo.setTask("Descricao");
 		todo.setDueDate(LocalDate.now());
 		controller.save(todo);
+		
 		Mockito.verify(taskRepo).save(todo);
 	}
-	
 }
